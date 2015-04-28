d3cf6c7151d5418372d1b9ab4db771a11e37f498
Derek George <derek68038@gmail.com>
2015-04-28 11:21:55 -0500

    Created migation for rounds table. Input field of round name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428161349) do
+ActiveRecord::Schema.define(version: 20150428162004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20150428161349) do
   create_table "players", force: :cascade do |t|
     t.string   "player_name"
     t.string   "position"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "rounds", force: :cascade do |t|
+    t.string   "round_name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
